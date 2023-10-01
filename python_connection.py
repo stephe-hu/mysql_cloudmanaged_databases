6d950ac211339a27e9295d92d4df697dd6dc9d90
stephe-hu <stephen.hu@stonybrook.edu>
2023-09-30 21:15:43 -0400

    added folders and files

---
+++ python_connection.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+from pandas import read_sql
+from sqlalchemy import create_engine, inspect
+
+
+# Load environment variables from .env file
+load_dotenv() 
+
+# Database connection settings from environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
+
+# Connection string
+conn_string = (
+    f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+    f"?charset={DB_CHARSET}"
+)
+
+# Create a database engine
+db_engine = create_engine(conn_string, echo=False)
+
+
+def get_tables(engine):
+    """Get list of tables."""
+    inspector = inspect(engine)
+    return inspector.get_table_names()
+
+def execute_query_to_dataframe(query: str, engine):
+    """Execute SQL query and return result as a DataFrame."""
+    return read_sql(query, engine)
+
+
+# Example usage
+tables = get_tables(db_engine)
+print("Tables in the database:", tables)
+
+sql_query = "SELECT * FROM your_table_name"  # Modify as per your table
+df = execute_query_to_dataframe(sql_query, db_engine)
+print(df)
