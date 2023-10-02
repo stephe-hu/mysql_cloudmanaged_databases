8f433cf61a8cf236388fc9017ef21fe1081b9c1f
stephe-hu <stephen.hu@stonybrook.edu>
2023-10-01 21:33:55 -0400

    reorganized code

--- populate.py
+++ populate.py
@@ -40,6 +40,15 @@ def execute_query_to_dataframe(query: str, engine):
 def insert_fake_data(engine, num_providers=8, num_patients=100, num_medications=20, num_patient_medications=150, num_demographics=20):
     # Start a connection
     with engine.connect() as connection:
+        
+        # Fetch all patient IDs, medication IDs, and provider IDs
+        query = text("SELECT patient_id FROM patients")
+        patient_ids = [row[0] for row in connection.execute(query).fetchall()]
+        query2 = text("SELECT medication_id FROM medications")
+        medication_ids = [row[0] for row in connection.execute(query2).fetchall()]
+        query3 = text("SELECT provider_id FROM providers")
+        provider_ids = [row[0] for row in connection.execute(query3).fetchall()]
+    
         # Insert fake data into providers
         for _ in range(num_providers):
             first_name = fake.first_name()
@@ -51,26 +60,19 @@ def insert_fake_data(engine, num_providers=8, num_patients=100, num_medications=
        
         # Insert fake data into patients
         for _ in range(num_patients):
+            patient_id: patient_id
             first_name = fake.first_name()
             last_name = fake.last_name()
             date_of_birth = fake.date_of_birth(minimum_age=10, maximum_age=90)
-            query = text("INSERT INTO patients (first_name, last_name, date_of_birth) VALUES "
-                         "(:first_name, :last_name, :date_of_birth)")
-            connection.execute(query, {"first_name": first_name, "last_name": last_name, "date_of_birth": date_of_birth})
+            primary_provider_id = random.choice(provider_ids)
+            query = text("INSERT INTO patients (first_name, last_name, date_of_birth, primary_provider_id) VALUES ""(:first_name, :last_name, :date_of_birth, :primary_provider_id)")
+            connection.execute(query, {"first_name": first_name, "last_name": last_name, "date_of_birth": date_of_birth, "primary_provider_id": primary_provider_id})
 
         # Insert sample medications into medications
         for medication in sample_medications:
             query = text("INSERT INTO medications (medication_name) VALUES (:medication_name)")
             connection.execute(query, {"medication_name": medication})
-
-        # Fetch all patient IDs, medication IDs, and provider IDs
-        query = text("SELECT patient_id FROM patients")
-        patient_ids = [row[0] for row in connection.execute(query).fetchall()]
-        query2 = text("SELECT medication_id FROM medications")
-        medication_ids = [row[0] for row in connection.execute(query2).fetchall()]
-        query3 = text("SELECT provider_id FROM providers")
-        provider_ids = [row[0] for row in connection.execute(query3).fetchall()]
-
+        
         # Insert fake data into patient_medications
         for _ in range(num_patient_medications):
             patient_id = random.choice(patient_ids)
@@ -101,6 +103,7 @@ def insert_fake_data(engine, num_providers=8, num_patients=100, num_medications=
                         "email": email
                     })
         connection.commit()    
+
 if __name__ == "__main__":
     insert_fake_data(db_engine)
     print("Fake data insertion complete!")
